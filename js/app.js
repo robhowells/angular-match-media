7e0b599bb7bca9e796a4d836fa288803518e1c4f
BLFILLEIGH\RHowells <rhowells@brayleino.co.uk>
2016-08-30 12:18:32 +0100

    set up project

---
+++ js/app.js
@@ -0,0 +1,26 @@
+(function() { 
+	var app = angular.module('matchMediaApp', ['matchmedia-ng']);
+
+	app.config(['matchmediaProvider', function (matchmediaProvider) {
+		matchmediaProvider.rules.phone = "(max-width: 759px)";
+		matchmediaProvider.rules.tablet = "(min-width: 760px) and (max-width: 1023px)";
+		matchmediaProvider.rules.desktop = "(min-width: 1024px)";
+	}]);
+
+	app.controller('MatchMediaController', ['matchmedia', function (matchmedia) {
+		var self = this,
+			device = {};
+
+	      device['phone'] = matchmedia.onPhone(function(mediaQueryList) {
+	          self.isPhone = mediaQueryList.matches;
+	      });
+	      device['tablet'] = matchmedia.onTablet( function(mediaQueryList) {
+	          self.isTablet = mediaQueryList.matches;
+	      });
+	      device['desktop'] = matchmedia.onDesktop( function(mediaQueryList) {
+	          self.isDesktop = mediaQueryList.matches;
+	      });
+
+  	}]);
+
+})();
